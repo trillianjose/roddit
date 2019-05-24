f0bdb8aec784820607240d7d475411a655752b49
trillianjose <trillianjosea@gmail.com>
2019-05-24 18:06:58 +0200

    cheking for errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,5 @@ class Post < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 80 }
   validates :body, presence: true, length: { maximum: 255 }
+  validates :user_id, presence: true
 end
