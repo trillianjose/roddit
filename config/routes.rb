ee7fc97d60509ddf7c171ab5d87547d20e9480aa
trillianjose <trillianjosea@gmail.com>
2019-05-27 12:13:36 +0200

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :comments
   resources :posts
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'posts#index'
 end
