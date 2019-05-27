c70116ca53963f04414c640a6397ece459921c8a
trillianjose <trillianjosea@gmail.com>
2019-05-27 12:06:27 +0200

    fix password on view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,14 @@
 class User < ApplicationRecord
-  has_many :post, dependent: :destroy
-  has_many :comment, dependent: :destroy
-
-  before_save { email.downcase! }
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-                    
+            format: { with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i },
+            uniqueness: { case_sensitive: false }
+
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  before_save { email.downcase! }
 end
